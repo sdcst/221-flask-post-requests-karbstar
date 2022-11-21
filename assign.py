f8e096d212f2b90f86a02288139b3fe83261916d
karbstar <90715867+karbstar@users.noreply.github.com>
2022-11-21 11:35:42 -0800

    done

--- assign.py
+++ assign.py
@@ -1,22 +1,19 @@
 #!python
-
 from flask import Flask,request
 from flask_cors import CORS
 import json
-
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def hello():
-    return "e"
-@app.route("/post",methods=["POST"])
+@app.route("/going",methods=["POST"])
 def postResponse():
     # a simple echo server that response with the payload received.
     payload = request.form
     data = dict(payload)
     print(data)
     return json.dumps(data)
-
+@app.route("/",methods=['POST','GET'])
+def main():
+    return "hi"
 
 app.run()
