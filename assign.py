48e86bdeca118d7e547cbe860413ecb152742510
karbstar <90715867+karbstar@users.noreply.github.com>
2022-11-22 11:35:41 -0800

    not

--- assign.py
+++ assign.py
@@ -9,11 +9,19 @@ CORS(app)
 def postResponse():
     # a simple echo server that response with the payload received.
     payload = request.form
+    print(request.form)
+    print(payload)
     data = dict(payload)
     print(data)
+    saf=data['get']
+    if saf!='bce52a22495d704233dd4e720e942dbf63d0561ed813d77fd38776357db7108e':
+        return 'incorect'
+    
+    print(saf)
+    
     return json.dumps(data)
 @app.route("/",methods=['POST','GET'])
 def main():
     return "hi"
 
-app.run()
+app.run("0.0.0.0")
